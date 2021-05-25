1a204c6c74115e748ec35bdaef7247b3d72bcac7
Michael Adams <mikelovesbooks@gmail.com>
2021-05-25 08:46:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,5 @@ setup(
   url = 'https://github.com/mikeAdamss/cmd-tools',
   download_url = 'https://github.com/mikeAdamss/cmd-tools/archive/0.0.1.tar.gz',
   keywords = ['databaker', 'pandas', 'cmd'],
-  classifiers = [],
-  entry_points={
-        'console_scripts': [
-            'cmdtools.commandline:cmdload'
-        ]
-  }
+  classifiers = []
 )
